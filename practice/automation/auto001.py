f94ee5e73e1d71fd0a4e462a526883345615bda0
weixy <weixiny2008@gmail.com>
2014-10-22 23:26:59 +1300

    v0.0.2

--- practice/automation/auto001.py
+++ practice/automation/auto001.py
@@ -7,7 +7,7 @@ import config.config_settings as settings
 auto_globals = {'proxy': None, 'selenium': None, 'server': None}
 
 
-def setup(logFile):
+def setup():
     global selenium
     server = Server(path=settings.BROWSERMOB_PROXY_PATH, options={'port': settings.BROWSERMOB_PROXY_PORT})
     server.start()
@@ -15,10 +15,11 @@ def setup(logFile):
     proxy.selenium_proxy()
     if settings.WEB_DRIVER == 'firefox':
         from selenium.webdriver.firefox.webdriver import WebDriver
+    elif settings.WEB_DRIVER == 'chrome':
+        from selenium.webdriver.chrome.webdriver import WebDriver
 
-        selenium = WebDriver(proxy=proxy)
+    selenium = WebDriver(proxy=proxy)
     logging.info('Automation configuration has been completed.')
-    proxy.new_har()
     return selenium, proxy, server
 
 
@@ -26,14 +27,14 @@ def execute(selenium, proxy, server):
     proxy.new_har("YELLOW")
     selenium.get(settings.SERVER_URL_TO_TEST)
     har = proxy.har
-    logFile.write('%s' % har)
+    harFile.write('%s' % har)
+    harFile.close()
     server.stop()
     selenium.quit()
 
 
 if __name__ == '__main__':
-    result_file = 'results/result.txt'
-    logFile = open(result_file, 'w')
-    auto_globals['selenium'], auto_globals['proxy'], auto_globals['server'] = setup(logFile)
+    har_file = 'results/yol.har'
+    harFile = open(har_file, 'w')
+    auto_globals['selenium'], auto_globals['proxy'], auto_globals['server'] = setup()
     execute(auto_globals['selenium'], auto_globals['proxy'], auto_globals['server'])
-    logFile.close()
