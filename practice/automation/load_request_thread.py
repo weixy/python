735480877837cfad3d5c9902a0f5d2ffd0390784
weixy <weixiny2008@gmail.com>
2014-11-25 15:33:43 +1300

    complete skeleton. Need to figure out what data to collect.

--- practice/automation/load_request_thread.py
+++ practice/automation/load_request_thread.py
@@ -13,9 +13,9 @@ TARGET_URL = {
 }
 
 # Configuration
-THREAD_NUM = 10
-WORKER_CYCLE = 50
-LOOP_SLEEP = 0.5
+THREAD_NUM = 5
+WORKER_CYCLE = 2
+LOOP_SLEEP = 0
 
 # Statistics
 ERROR_NUM = 0
@@ -33,7 +33,8 @@ def do_work(index, url):
         start = millis_now()
         response = client.get(refer_url)
         diff = millis_now() - start
-        print '%s ... ... %s' % (diff, response.status_code)
+        print '[' + t.name + '.' + str(index) + \
+              '.1] Extract token ... ' + '%s (ms) ... ... %s' % (diff, response.status_code)
         csrftoken = client.cookies['csrftoken']
 
         login_data = dict(username='developers@finda.co.nz', password='QAWSEDRFTG', csrfmiddlewaretoken=csrftoken)
@@ -41,13 +42,14 @@ def do_work(index, url):
         start = millis_now()
         response = client.post(login_url, data=login_data, headers=dict(Referer=refer_url), allow_redirects=False)
         diff = millis_now() - start
-        print '%s ... ... %s' % (diff, response.status_code)
+        print '[' + t.name + '.' + str(index) + '.2] Login ... ' + '%s (ms) ... ... %s' % (diff, response.status_code)
         redirect_url = response.headers['Location']
 
         start = millis_now()
         response = client.get(redirect_url, allow_redirects=False)
         diff = millis_now() - start
-        print '%s ... ... %s' % (diff, response.status_code)
+        print '[' + t.name + '.' + str(index) + \
+              '.3] Load page after login ... ' + '%s (ms) ... ... %s' % (diff, response.status_code)
 
         busi_url = '%s%s' % (url, '/business/6401/')
         start = millis_now()
@@ -56,10 +58,13 @@ def do_work(index, url):
         if response.status_code == 302 and 'Location' in response.headers:
             relocation = response.headers['Location']
             print relocation
-        print '%s ... ... %s' % (diff, response.status_code)
+        print '[' + t.name + '.' + str(index) + \
+              '.4] Open a business ... ' + '%s (ms) ... ... %s' % (diff, response.status_code)
 
     except Exception as e:
         print 'Failed with exception \'%s\': \n%s' % (e.__class__, e.message)
+        global ERROR_NUM
+        ERROR_NUM += 1
 
 
 def working():
@@ -76,16 +81,30 @@ def working():
 
 
 def main():
-    start = millis_now()
+    t1 = millis_now()
 
-    Threads = []
+    threads = []
 
     for i in range(THREAD_NUM):
         t = threading.Thread(target=working, name='T' + str(i))
-        t.setDaemon(True)
-        Threads.append(t)
+        t.daemon = True
+        threads.append(t)
 
+    for t in threads:
+        t.start()
 
+    for t in threads:
+        t.join()
+
+    print 'main thread end'
+
+    t2 = millis_now()
+    print '=========================================='
+    print 'Task Number: ', THREAD_NUM, '*', WORKER_CYCLE, '=', THREAD_NUM * WORKER_CYCLE
+    print 'Total Time (ms): ', t2 - t1
+    print 'Average Time per Request (ms): ', (t2 - t1) / (THREAD_NUM * WORKER_CYCLE)
+    print 'Requests per Second: ', 1.0 / ((t2 - t1) / THREAD_NUM * WORKER_CYCLE) * 1000
+    print 'Error Number: ', ERROR_NUM
 
 if __name__ == '__main__':
     main()
