26843cf64d8480a681a7500021d2cf739446563e
weixy <weixiny2008@gmail.com>
2014-12-17 23:04:23 +1300

    modify view of vote

--- mypoll/polls/views.py
+++ mypoll/polls/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
-from models import Question
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from models import Question, Choice
 
 
 # Create your views here.
@@ -24,4 +25,15 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/index.html', {
+            'question': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
