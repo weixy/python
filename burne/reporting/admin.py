ab4173f8e7c510c70aef24ca27444a6a33e8bf51
weixy <weixiny2008@gmail.com>
2014-12-22 17:45:11 +1300

    start to build models/admin views for scenario, request.

--- burne/reporting/admin.py
+++ burne/reporting/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from models import Scenario, LoadRequest
 
-# Register your models here.
+admin.site.register(Scenario)
+admin.site.register(LoadRequest)
--- burne/reporting/models.py
+++ burne/reporting/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Scenario(models.Model):
+    name = models.CharField(max_length=256)
+    domain = models.CharField(max_length=256)
+
+
+class LoadRequest(models.Model):
+    scenario = models.ForeignKey(Scenario)
+    url = models.CharField(max_length=256, default='/')
+    type = models.CharField(max_length=10, default='GET')
+    data = models.CharField(max_length=256, default=None)
+    header = models.CharField(max_length=256, default=None)
+    redirects = models.BooleanField(default=False)
