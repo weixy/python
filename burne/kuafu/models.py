4c6294c6c6e63b90e6caca149f44391c373b7295
weixy <weixiny2008@gmail.com>
2014-12-23 13:07:09 +1300

    add app kuafu, which will be portal of load testing.

--- burne/kuafu/models.py
+++ burne/kuafu/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=256, default=None)
+    description = models.TextField(max_length=512, default=None)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Domain(models.Model):
+    product = models.ForeignKey(Product)
+    name = models.CharField(max_length=256, default=None)
+    url = models.CharField(max_length=256, default=None)
+
+    def __unicode__(self):
+        return self.name
